4c736afb36b9d4c525d9e3e9dddec63b1382f77c
Thulasiram <thulasi29n@gmail.com>
2021-10-02 00:26:48 +0530

    Update RouteTable.tf

--- RouteTable.tf
+++ RouteTable.tf
@@ -1,16 +1,15 @@
 resource "aws_route_table" "Project_RT" {
   vpc_id = aws_vpc.Project_Vpc.id
 
-  route = [
-        {
-          cidr_block = "0.0.0.0/0"
-          gateway_id = aws_internet_gateway.Project_GW.id
-        },
-        {
-          ipv6_cidr_block        = "::/0"
-          egress_only_gateway_id=aws_internet_gateway.Project_GW.id
-        }
-      ]
+  route{
+      cidr_block = "0.0.0.0/0" 
+      gateway_id = aws_internet_gateway.Project_GW.id
+    }
+    route {
+    
+      ipv6_cidr_block        = "::/0"
+      gateway_id = aws_internet_gateway.Project_GW.id
+    }
 
 
   tags = {
